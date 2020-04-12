b80e0d79e4b47dda143cf0aa6a73abed4f0ec266
wincerd <egatehi@gmail.com>
2020-04-12 21:30:13 +0300

    added mql5 zeromq api

---
+++ Metatrader_API.py
@@ -0,0 +1,108 @@
+import zmq
+
+
+
+
+class MTraderAPI:
+    def __init__(self, host=None):
+        self.HOST = host or 'localhost'
+        self.SYS_PORT = 15555       # REP/REQ port
+        self.DATA_PORT = 15556      # PUSH/PULL port
+        self.LIVE_PORT = 15557      # PUSH/PULL port
+        self.EVENTS_PORT = 15558    # PUSH/PULL port
+
+        # ZeroMQ timeout in seconds
+        sys_timeout = 1
+        data_timeout = 10
+
+        # initialise ZMQ context
+        context = zmq.Context()
+
+        # connect to server sockets
+        try:
+            self.sys_socket = context.socket(zmq.REQ)
+            # set port timeout
+            self.sys_socket.RCVTIMEO = sys_timeout * 1000
+            self.sys_socket.connect('tcp://{}:{}'.format(self.HOST, self.SYS_PORT))
+
+            self.data_socket = context.socket(zmq.PULL)
+            # set port timeout
+            self.data_socket.RCVTIMEO = data_timeout * 1000
+            self.data_socket.connect('tcp://{}:{}'.format(self.HOST, self.DATA_PORT))
+        except zmq.ZMQError:
+            raise zmq.ZMQBindError("Binding ports ERROR")
+
+    def _send_request(self, data: dict) -> None:
+        """Send request to server via ZeroMQ System socket"""
+        try:
+            self.sys_socket.send_json(data)
+            msg = self.sys_socket.recv_string()
+            # terminal received the request
+            assert msg == 'OK', 'Something wrong on server side'
+        except AssertionError as err:
+            raise zmq.NotDone(err)
+        except zmq.ZMQError:
+            raise zmq.NotDone("Sending request ERROR")
+
+    def _pull_reply(self):
+        """Get reply from server via Data socket with timeout"""
+        try:
+            msg = self.data_socket.recv_json()
+        except zmq.ZMQError:
+            raise zmq.NotDone('Data socket timeout ERROR')
+        return msg
+
+    def live_socket(self, context=None):
+        """Connect to socket in a ZMQ context"""
+        try:
+            context = context or zmq.Context.instance()
+            socket = context.socket(zmq.PULL)
+            socket.connect('tcp://{}:{}'.format(self.HOST, self.LIVE_PORT))
+        except zmq.ZMQError:
+            raise zmq.ZMQBindError("Live port connection ERROR")
+        return socket
+
+    def streaming_socket(self, context=None):
+        """Connect to socket in a ZMQ context"""
+        try:
+            context = context or zmq.Context.instance()
+            socket = context.socket(zmq.PULL)
+            socket.connect('tcp://{}:{}'.format(self.HOST, self.EVENTS_PORT))
+        except zmq.ZMQError:
+            raise zmq.ZMQBindError("Data port connection ERROR")
+        return socket
+
+    def construct_and_send(self, **kwargs) -> dict:
+        """Construct a request dictionary from default and send it to server"""
+
+        # default dictionary
+        request = {
+            "action": None,
+            "actionType": None,
+            "symbol": None,
+            "chartTF": None,
+            "fromDate": None,
+            "toDate": None,
+            "id": None,
+            "magic": None,
+            "volume": None,
+            "price": None,
+            "stoploss": None,
+            "takeprofit": None,
+            "expiration": None,
+            "deviation": None,
+            "comment": None
+        }
+
+        # update dict values if exist
+        for key, value in kwargs.items():
+            if key in request:
+                request[key] = value
+            else:
+                raise KeyError('Unknown key in **kwargs ERROR')
+
+        # send dict to server
+        self._send_request(request)
+
+        # return server reply
+        return self._pull_reply()
